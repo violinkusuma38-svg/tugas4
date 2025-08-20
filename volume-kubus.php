0f784e7dc0965d5c4b3753ea903495e56007b54f
violinkusuma38-svg <violin.kusuma38@smk.belajar.id>
2025-08-20 18:43:59 +0700

    kubuslagi

---
+++ volume-kubus.php
@@ -0,0 +1,10 @@
+<?php
+echo "<h1>Volume Kubus</h1>";
+
+$sisi = 4;
+$volume = pow($sisi, 3);
+
+echo "<b>Rumus:</b> V = s³ <br>";
+echo "<b>Soal:</b> Hitung volume kubus dengan panjang sisi $sisi cm.<br>";
+echo "<b>Jawaban:</b> $sisi³ = $volume cm³";
+?>
