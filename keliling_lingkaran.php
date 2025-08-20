00c8fe33121467060abf6220f2031c3a94b666ca
violinkusuma38-svg <violin.kusuma38@smk.belajar.id>
2025-08-20 16:34:24 +0700

    first commit

---
+++ keliling_lingkaran.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h1>Luas Persegi Panjang</h1>";
+
+$panjang = 10;
+$lebar   = 5;
+$luas = $panjang * $lebar;
+
+echo "<b>Rumus:</b> L = p × l <br>";
+echo "<b>Soal:</b> Hitung luas persegi panjang dengan panjang $panjang cm dan lebar $lebar cm.<br>";
+echo "<b>Jawaban:</b> $panjang × $lebar = $luas cm²";
+?>
